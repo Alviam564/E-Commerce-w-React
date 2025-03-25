b5e67ddb7e781cb5b947a46d52692b1f0d1e8e48
Alviam564 <alvarezwilliam543@gmail.com>
2025-03-25 09:44:27 -0400

    update 0.1 3/25

--- src/components/ui/book.jsx
+++ src/components/ui/book.jsx
@@ -1,4 +1,4 @@
-import React, { useState }from "react";
+import React, { useEffect, useRef, useState }from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Price from "./Price";
